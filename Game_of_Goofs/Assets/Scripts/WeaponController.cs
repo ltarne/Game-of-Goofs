c528eac735fb71902398c1470e2875e24e97e050
Luke Burton <lhburton23@gmail.com>
2018-01-14 17:00:17 +0000

    Added arms and a shield courtesy of matt

--- Game_of_Goofs/Assets/Scripts/WeaponController.cs
+++ Game_of_Goofs/Assets/Scripts/WeaponController.cs
@@ -6,6 +6,9 @@ public class WeaponController : MonoBehaviour {
     [SerializeField]
     float m_WeaponPush = 1.0f;
 
+    [SerializeField]
+    float m_WeaponAngle = 90.0f;
+
 
 
     // Use this for initialization
@@ -15,6 +18,14 @@ public class WeaponController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        if (Input.GetKeyDown("e"))
+        {
+            Hit();
+        }
+    }
+
+    void Hit()
+    {
+        transform.RotateAround(transform.position, Vector3.left, m_WeaponAngle);
+    }
 }
