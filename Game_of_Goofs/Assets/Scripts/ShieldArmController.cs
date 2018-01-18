a5397c5b74a883842201404679675ad72f62dd3f
Luke Burton <lhburton23@gmail.com>
2018-01-18 00:56:37 +0000

    Hammer and shield raise

---
+++ Game_of_Goofs/Assets/Scripts/ShieldArmController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShieldArmController : MonoBehaviour {
+
+    
+
+    [SerializeField]
+    public float m_RaisedAngle = -60;
+
+    [SerializeField]
+    public float m_RaiseSpeed = 5.0f;
+
+    private float m_Counter = 0;
+    private bool m_ShieldUp = false;
+
+	
+	// Update is called once per frame
+	void Update () {
+
+        if(Input.GetKeyDown("q"))
+        {
+            m_ShieldUp = true;
+        }
+
+        if(m_ShieldUp)
+        {
+            if(m_Counter < -m_RaisedAngle)
+            {
+                transform.RotateAround(transform.parent.position, Vector3.left, -m_RaiseSpeed);
+                m_Counter += 5;
+            }
+            else if(m_Counter >= -m_RaisedAngle && m_Counter <= 2 * -m_RaisedAngle)
+            {
+                transform.RotateAround(transform.parent.position, Vector3.left, m_RaiseSpeed);
+                m_Counter += 5;
+            }
+            else
+            {
+                m_Counter = 0;
+                m_ShieldUp = false;
+            }
+
+        }
+		
+	}
+
+}
---
+++ Game_of_Goofs/Assets/Scripts/WeaponArmController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponArmController : MonoBehaviour {
+
+    [SerializeField]
+    public float m_RaisedAngle = -60;
+
+    [SerializeField]
+    public float m_RaiseSpeed = 5.0f;
+
+    private bool m_Attack = false;
+    private float m_Counter = 0;
+
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown("e"))
+        {
+            m_Attack = true;
+        }
+
+        if (m_Attack)
+        {
+            if (m_Counter < -m_RaisedAngle)
+            {
+                transform.RotateAround(transform.parent.position, Vector3.left, -m_RaiseSpeed);
+                m_Counter += 5;
+            }
+            else if (m_Counter >= -m_RaisedAngle && m_Counter <= (2 * -m_RaisedAngle))
+            {
+                transform.RotateAround(transform.parent.position, Vector3.left, m_RaiseSpeed);
+                m_Counter += 5;
+            }
+            else
+            {
+                m_Counter = 0;
+                m_Attack = false;
+            }
+
+        }
+    }
+}
